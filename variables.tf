9df222c7a0b6f8a9d36fa335a2ceecf7e8273a9a
HolyKadylo <pivas47@gmail.com>
2021-11-24 17:08:49 +0200

    Subnetting task (#1)
    * subnetting
    
    * fixing typo
    
    * subnetting
    
    * subnetting
    
    * subnetting
    
    * subnetting
    
    * subnetting
    
    * subnetting
    
    * subnetting
    
    * subnetting
    
    * subnetting

--- variables.tf
+++ variables.tf
@@ -58,3 +58,27 @@ variable "environment" {
   type        = string
   default     = "env"
 }
+
+variable "CIDR1" {
+  description = "CIDR for private network"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "CIDR2" {
+  description = "CIDR for private network"
+  type        = string
+  default     = "10.0.2.0/24"
+}
+
+variable "CIDR3" {
+  description = "CIDR for public network"
+  type        = string
+  default     = "10.0.3.0/24"
+}
+
+variable "CIDR4" {
+  description = "CIDR for public network"
+  type        = string
+  default     = "10.0.4.0/24"
+}
